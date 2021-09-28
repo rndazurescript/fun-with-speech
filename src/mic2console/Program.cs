c98d84653d5e1809bbc0a3362a9e48d3b8925d66
R&D Azure Script <rndazurescript@users.noreply.github.com>
2021-09-28 18:38:30 +0300

    Added mic2console sample

---
+++ src/mic2console/Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using System;
+using System.Threading.Tasks;
+
+namespace mic2console
+{
+    class Program
+    {
+        public static async Task RecognizeSpeechAsync()
+        {
+            // Creates an instance of a speech config using local containers
+            // https://docs.microsoft.com/azure/cognitive-services/speech-service/speech-container-howto
+            var config = SpeechConfig.FromHost(new Uri("ws://localhost:5000"));
+            // If you want to use an Azure subscription directly, use:
+            // var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+
+            // Optionally, change default language. Get list of supported languages
+            // https://docs.microsoft.com/azure/cognitive-services/speech-service/language-support#speech-to-text
+            // config.SpeechRecognitionLanguage = "en-US";
+
+            // To enable logs
+            // https://docs.microsoft.com/azure/cognitive-services/speech-service/how-to-use-logging
+            // config.SetProperty(PropertyId.Speech_LogFilename, @"c:\tmp\mic2console.log");
+
+
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                Console.WriteLine("Say something... Say a sentence that contains the word stop to stop transcribing.");
+                var stopRecognition = new TaskCompletionSource<int>();
+                recognizer.Recognizing += (s, e) =>
+                {
+                    Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
+                };
+
+                recognizer.Recognized += async (s, e) =>
+                {
+                    if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                    {
+                        Console.WriteLine($"> {e.Result.Text}");
+                        if (e.Result.Text.Contains("stop"))
+                        {
+                            await recognizer.StopContinuousRecognitionAsync();
+                        }
+                    }
+                    else if (e.Result.Reason == ResultReason.NoMatch)
+                    {
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    }
+                };
+
+                recognizer.Canceled += (s, e) =>
+                {
+                    Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+                    if (e.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the speech key and location/region info?");
+                    }
+
+                    stopRecognition.TrySetResult(0);
+                };
+
+                recognizer.SessionStopped += (s, e) =>
+                {
+                    Console.WriteLine("\n    Session stopped event.");
+                    stopRecognition.TrySetResult(0);
+                };
+
+                await recognizer.StartContinuousRecognitionAsync();
+                // Waits for completion. Use Task.WaitAny to keep the task rooted.
+                Task.WaitAny(new[] { stopRecognition.Task });
+            }
+        }
+
+        static async Task Main()
+        {
+            await RecognizeSpeechAsync();
+            Console.WriteLine("Please press <Return> to continue.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/mic2console/README.md
@@ -0,0 +1,12 @@
+﻿# Microphone to console transcription
+
+Simple application that uses the `StartContinuousRecognitionAsync` to transcribe text from microphone. 
+Say a sentence that contains the `stop` word to exit cancel the transcription session.
+
+To spin up the local container [follow official guidance](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/speech-container-howto). The basic idea is:
+
+```
+docker run --rm -it -p 5000:5000 --memory 4g --cpus 4 mcr.microsoft.com/azure-cognitive-services/speechservices/speech-to-text:latest Eula=accept Billing=https://{region}.api.cognitive.microsoft.com/sts/v1.0/issuetoken ApiKey={speech service key}
+```
+
+You can also use Azure Resources directly by using `SpeechConfig.FromSubscription` instead of the `SpeechConfig.FromHost` used in this sample.
